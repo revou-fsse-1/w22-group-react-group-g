d3f38c98b5a667e839e676d27df67cb778ab9451
Ichsan Zulfikar <ichsanzulfikar2141@gmail.com>
2023-07-14 21:22:42 +0700

    test

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: { email: "admin@admin.com" },
+    update: {},
+    create: {
+      name: "Admin",
+      email: "admin@admin.com",
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit();
+  });
---
+++ src/app/new/page.tsx
@@ -0,0 +1,9 @@
+import ListUsers from "@/components/listusers.component";
+
+export default async function Home() {
+  return (
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+      <ListUsers />
+    </main>
+  );
+}
