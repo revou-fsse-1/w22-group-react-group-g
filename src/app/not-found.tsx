6e85d258fc382ad63e474dc3e918c24737353aa8
Derian Melvin <derianmelvin88@gmail.com>
2023-07-21 00:21:02 +0700

    Update not found page styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,35 @@
+import Logo from "@/components/Logo";
 import Link from "next/link";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center p-12 text-gray-100 bg-[#0E141A] md:p-16">
-      <div className="w-full max-w-7xl flex flex-col gap-11 md:gap-14">
-        <Header />
-        <main className="flex-1 flex flex-col items-center justify-center">
-          <h2 className="text-4xl font-bold mb-4">404 - Page Not Found</h2>
-          <p className="text-lg mb-4">
-            Oops! The page you are looking for does not exist.
-          </p>
-          <p className="text-lg">
-            Go back to
-            <Link href="/">
-              <span> Home</span>
-            </Link>
-          </p>
+      <div className="w-full max-w-7xl flex flex-col gap-24 items-center">
+        <div className="flex items-center gap-5">
+          <Logo width="78" height="72" />
+          <span className="text-4xl font-bold text-[#1371FF]">Minicrate</span>
+        </div>
+
+        <main className="flex flex-col items-center gap-3">
+          <h2 className="text-8xl font-bold text-[#222E3F] sm:text-9xl hover:text-[#1371FF] transition-all duration-300">
+            404
+          </h2>
+          <p className="text-2xl sm:text-3xl">Page Not Found</p>
         </main>
-        <Footer />
+
+        <div className="flex flex-col items-center gap-5">
+          <Link href={"/"}>
+            <button className="w-full px-5 py-4 rounded-lg font-semibold bg-[#0051CC] bg-opacity-60 md:text-lg xl:text-xl hover:bg-opacity-100 transition-all">
+              Back to Homepage
+            </button>
+          </Link>
+
+          <Link href={"/login"}>
+            <button className="w-full px-5 py-4 rounded-lg bg-[#222E3F] bg-opacity-30 md:text-lg xl:text-xl hover:bg-opacity-80 transition-all">
+              Go to Login
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
